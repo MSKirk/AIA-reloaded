029413e79ae8ac3342ef25c605410b1a180a96c2
Michael Kirk <michael.s.kirk@nasa.gov>
2019-03-05 17:34:00 -0500

    Updated class structure and added header corrections

--- calibration.py
+++ calibration.py
@@ -1,9 +1,97 @@
 import numpy as np
 import cv2
-from astropy.io import fits
+from sunpy.io import fits
 
-# The aia image size is fixed by the size of the detector. For AIA raw data, this has no reason to change.
-aia_image_size = 4096
+
+class AIAPrep:
+    # Promoting to the level 1.5 standard
+    # 1) Plate scale, 2)rotation & 3)shift
+    # 4) Rescale to integer
+    # See http://jsoc.stanford.edu/doc/data/aia_test/SDOD0045_v10_AIA_plan_for_producing_and_distri_AIADataP_gPlanv1-6.pdf
+
+    def __init__(self, filename, level=1.5, cropsize=None):
+        # The aia image size is fixed by the size of the detector. For AIA raw data, this has no reason to change.
+        self.aia_image_size = 4096
+        self.cropsize = cropsize
+        self.data, self.header = aia_fits_read(filename)
+
+        self.aiaprep()
+        self.aia_lev15_header_update()
+
+
+        if cropsize:
+            self.crop_image()
+
+    def aiaprep(self):
+        self.data /= self.header['EXPTIME']
+        # Target scale is 0.6 arcsec/px
+        target_scale = 0.6
+        scale_factor = self.header['CDELT1'] / target_scale
+        # Center of rotation at reference pixel converted to a coordinate origin at 0
+        reference_pixel = [self.header['CRPIX1'] - 1, self.header['CRPIX2'] - 1]
+        # Rotation angle with openCV uses coordinate origin at top-left corner. For solar images in numpy we need to invert the angle.
+        angle = -self.header['CROTA2']
+        # Run scaled rotation. The output will be a rotated, rescaled, padded array.
+        prepdata = scale_rotate(self.data, angle=angle, scale_factor=scale_factor,
+                                reference_pixel=reference_pixel)
+        prepdata[prepdata < 0] = 0
+
+        self.data = np.int32(prepdata)
+
+    def aia_lev15_header_update(self):
+        # update the header for level 1.5 corrections
+
+        #plate scale
+        self.header['CDELT1'] = 0.6
+        self.header['CDELT2'] = 0.6
+
+        #center pixel
+        self.header['CRPIX1'] = 2048.5
+        self.header['CRPIX2'] = 2048.5
+
+        #Sun Size
+        self.header['r_sun'] = self.header['rsun_obs'] / self.header['cdelt1']
+
+        #Level Number
+        self.header['lvl_num'] = 1.5
+
+        #bits per pixel
+        self.header['bitpix'] = -64
+
+        #Define rotation matrix
+        self.header['pc2_1'] = 0.0
+        self.header['pc1_2'] = 0.0
+        self.header['pc2_2'] = 1.0
+        self.header['pc1_1'] = 1.0
+
+        # Data Values
+        self.header['DATAMIN'] = self.data.min()
+
+        self.header['DATAMAX'] = self.data.max()
+        self.header['DATAMEDN'] = np.median(self.data)
+        self.header['DATAMEAN'] = self.data.mean()
+        self.header['DATARMS'] = np.std(self.data)
+
+    def crop_image(self):
+        center = ((np.array(self.data.shape) - 1) / 2.0).astype(int)
+        half_size = int(self.cropsize / 2)
+        self.data = self.data[center[1] - half_size:center[1] + half_size,
+                   center[0] - half_size:center[0] + half_size]
+
+        #new center pixel
+        self.header['CRPIX1'] = self.data.shape[0]/2 + 0.5
+        self.header['CRPIX2'] = self.data.shape[1]/2 + 0.5
+
+
+    # FUTURE --->> update the header for level 1.6 corrections
+    #def aia_lev16_header_update(self):
+
+    # Corrections to the level 1.6 standard
+    # PSF and diffraction correction
+    # Effective Area correction
+    # Rescale to integer; saved as 32bit Int
+
+    #def aia16prep(self):
 
 def scale_rotate(image, angle=0, scale_factor=1, reference_pixel=None):
     """
@@ -56,33 +144,6 @@ def scale_rotate(image, angle=0, scale_factor=1, reference_pixel=None):
 
     return rotated_image
 
-# Promoting to the level 1.5 standard
-# 1) Plate scale, 2)rotation & 3)shift
-# 4) Rescale to integer
-# See http://jsoc.stanford.edu/doc/data/aia_test/SDOD0045_v10_AIA_plan_for_producing_and_distri_AIADataP_gPlanv1-6.pdf
-def aiaprep(fitsfile, cropsize=aia_image_size):    
-    data, header = aia_fits_read(fitsfile)
-    
-    data /= header['EXPTIME']
-    # Target scale is 0.6 arcsec/px
-    target_scale = 0.6
-    scale_factor = header['CDELT1'] / target_scale
-    # Center of rotation at reference pixel converted to a coordinate origin at 0
-    reference_pixel = [header['CRPIX1'] - 1, header['CRPIX2'] - 1]
-    # Rotation angle with openCV uses coordinate origin at top-left corner. For solar images in numpy we need to invert the angle.
-    angle = -header['CROTA2']
-    # Run scaled rotation. The output will be a rotated, rescaled, padded array.
-    prepdata = scale_rotate(data, angle=angle, scale_factor=scale_factor, reference_pixel=reference_pixel)
-    prepdata[prepdata < 0] = 0
-
-    if cropsize is not None:
-        center = ((np.array(prepdata.shape) - 1) / 2.0).astype(int)
-        half_size = int(cropsize / 2)
-        prepdata = prepdata[center[1] - half_size:center[1] + half_size, center[0] - half_size:center[0] + half_size]
-
-    return prepdata
-
-
 # Alternate padding method. On AIA, it is ~6x faster than numpy.pad used in Sunpy's aiaprep
 def aia_pad(image, pad_x, pad_y):
     newsize = [image.shape[0]+2*pad_y, image.shape[1]+2*pad_x]
@@ -98,35 +159,17 @@ def aia_pad(image, pad_x, pad_y):
 def aia_fits_read(fitsfile):
 
     hdul = fits.open(fitsfile)
-    hdul[1].verify('silentfix')
-    header = hdul[1].header
-    data = hdul[1].data.astype(np.float64)
+    for ii in range(len(hdul)):
+        if isinstance(hdul[ii].data,  np.ndarray):
+            hdul[ii].verify('silentfix')
+            header = hdul[ii].header
+            data = hdul[ii].data.astype(np.float64)
+
+            return data, header
+
+
+
+
     
-    return data, header
-
-# fits write for aia images
-def aia_fits_write(fname, data, header, hdu_type=None):
-
-
-# update the header for level 1.5 corrections
-def aia_lev15_header_update(header):
-
-
-
-    return updated_header
-    
-# update the header for level 1.6 corrections
-def aia_lev16_header_update(header):
-
-    return updated_header
-    
-# Corrections to the level 1.6 standard
-# PSF and diffraction correction 
-# Effective Area correction
-# Rescale to integer; saved as 32bit Int
-
-def aia16prep(fitsfile, cropsize=aia_image_size):
-
-    return prepdata
 
 
